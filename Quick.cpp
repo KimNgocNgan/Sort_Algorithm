fe1f9149dc15d89a3af09f25d4bddab5a2f6a507
Kim Ngọc Ngân <120255766+KimNgocNgan@users.noreply.github.com>
2023-03-12 22:46:22 +0700

    Add files via upload

---
+++ Quick.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+const int n = 1000000;
+float a[1000005];
+
+void quickSort(float a[], int left, int right) 
+{
+	int i = left, j = right;
+	float pivot = a[left + rand() % (right - left)];
+	
+	while (i <= j) 
+    {
+		while (a[i] < pivot) i++;
+		while (a[j] > pivot) j--;
+
+		if (i <= j) {
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}
+	if (left < j) quickSort(a, left, j);
+	if (i < right) quickSort(a, i, right);
+}
+
+int main() 
+{
+    for (int Test = 1; Test <= 10; ++Test) 
+    {
+        string file_name = "input/test" + to_string(Test) + ".inp";
+        ifstream fi(file_name);
+        for (int i = 0; i < n; ++i) fi >> a[i];
+        auto start_time = high_resolution_clock::now();
+        quickSort(a, 0, n-1);
+        auto end_time = high_resolution_clock::now();
+        cout << duration_cast<microseconds>(end_time - start_time).count() <<'\n';
+    }
+    return 0;
+}
---
+++ crease_inp.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+#define rand rd
+const int size_array = 1000000; 
+int num[size_array + 5];
+
+void decreasing_num() 
+{
+    ofstream fo("test1.inp"); 
+    for (int i = 0; i < size_array; i++ )
+        num[i] = rand(); 
+    sort(num, num + size_array, greater<int>() ); 
+    for (int i = 0; i < size_array; i++ )
+        fo << num[i] << ' ';
+    fo.close();
+}
+
+void increasing_num() 
+{
+    ofstream fo ("test2.inp");  
+    for (int i = 0; i < size_array; i++ )
+        num[i] = rand();
+    sort(num, num + size_array );
+    for (int i = 0; i < size_array; i++ )
+        fo << num[i] << ' ';
+    fo.close();
+}
+
+void random_num() 
+{
+    for (int i = 3; i <= 10; i++ ) 
+    {
+        string file_name = "test" + to_string(i) + ".inp"; 
+        ofstream fo (file_name); 
+        for (int i = 0; i < size_array; i++ )
+            fo << rand() << ' ';
+        fo.close();
+    }
+}
+
+int main() 
+{
+    decreasing_num(); 
+    increasing_num();
+    random_num();
+    return 0;
+}
